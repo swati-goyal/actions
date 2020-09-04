826883b417f7af0640e51c48ad3479cfa188590f
swati.goyal <swati_goyal@outlook.com>
2020-09-04 13:10:55 +0530

    Added scehdule and sleep time, renamed workflow

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 from seleniumbase import BaseCase
+import time
+
 
 class MyTestClass(BaseCase):
 
     def test_demo_site(self):
+
         url = "https://madrid.quintype.io/"
         path = "ampstories/visual-stories/why-you-need-a-dog-in-your-life"
         self.open(url+path)
+
+        time.sleep(20)
