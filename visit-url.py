4b55af9f7d830678a65ed5e92af017bcde5c48b8
swati.goyal <swati_goyal@outlook.com>
2020-09-04 11:58:12 +0530

    Fixed lint errors

--- visit-url.py
+++ visit-url.py
@@ -1,11 +1,15 @@
 import unittest
 import requests
 
+
 class TestStringMethods(unittest.TestCase):
+
     def visit_url(self):
+
         url = "https://madrid.quintype.io/"
         path = "ampstories/visual-stories/why-you-need-a-dog-in-your-life"
         requests.get(url+path)
+
 
 if __name__ == '__main__':
     unittest.main()
