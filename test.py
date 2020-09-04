d9aba3c18ff71e4365efbf977aed7b271db8656c
swati.goyal <swati_goyal@outlook.com>
2020-09-04 13:49:37 +0530

    Added new tech urls

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ class MyTestClass(BaseCase):
     def test_demo_site(self):
 
         url = "https://madrid.quintype.io/"
-        path = "ampstories/visual-stories/why-you-need-a-dog-in-your-life"
+        path = "technology/drivezy-to-liquidate-its-fleet-as-demand-falls-amid-virus-outbreak"
         path1 = "culture/india/pursuitsbugatti-centodieci-revives-spirit-of-eb110-photos-specs-price"
         path2 = "tech/mobile/trai-chief-we-set-spectrum-price-industry-decides-tariff"
         path3 = "culture/this-is-a-title-9"
-        path4 = "ampstories/technology/visual-story-demo1"
+        path4 = "technology/youtube-now-available-for-jiophone"
 
         self.open(url+path)
         time.sleep(5)
