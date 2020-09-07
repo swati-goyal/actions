7f98a26700c073b966e4b82da565da8f6e2df092
Swati Goyal <swati_goyal@outlook.com>
2020-09-07 16:13:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,13 +5,18 @@ import time
 class MyTestClass(BaseCase):
 
     def test_demo_site(self):
-
+        
+        # Prod
         url = "https://madrid.quintype.io/"
         path = "technology/drivezy-to-liquidate-its-fleet-as-demand-falls-amid-virus-outbreak"
         path1 = "culture/india/pursuitsbugatti-centodieci-revives-spirit-of-eb110-photos-specs-price"
         path2 = "tech/mobile/trai-chief-we-set-spectrum-price-industry-decides-tariff"
         path3 = "culture/this-is-a-title-9"
         path4 = "technology/youtube-now-available-for-jiophone"
+        
+        # Staging
+        url_stg = "https://bloombergquint-web.qtstage.io/"
+        path5 = "bq-blue-exclusive/new-story-check-for-add-social-card"
 
         self.open(url+path)
         time.sleep(5)
@@ -26,4 +31,7 @@ class MyTestClass(BaseCase):
         time.sleep(5)
 
         self.open(url+path4)
+        time.sleep(5)
+        
+        self.open(url_stg+path5)
         time.sleep(5)
