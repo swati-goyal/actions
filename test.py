04b327161d1773aebb0e78bdfebac738d08050d5
Swati Goyal <swati_goyal@outlook.com>
2021-11-10 17:30:19 +0530

    Added open url for staging website, some refactor

--- test.py
+++ test.py
@@ -3,35 +3,25 @@ import time
 
 
 class MyTestClass(BaseCase):
+    
+    def open_url(self, url, path):
+        self.open(url + path)
+        time.sleep(1)
 
     def test_demo_site(self):
         
         # Prod
         url = "https://madrid.quintype.io/"
-        path = "technology/drivezy-to-liquidate-its-fleet-as-demand-falls-amid-virus-outbreak"
-        path1 = "culture/india/pursuitsbugatti-centodieci-revives-spirit-of-eb110-photos-specs-price"
-        path2 = "tech/mobile/trai-chief-we-set-spectrum-price-industry-decides-tariff"
-        path3 = "culture/this-is-a-title-9"
-        path4 = "technology/youtube-now-available-for-jiophone"
+        prod_paths = ["technology/drivezy-to-liquidate-its-fleet-as-demand-falls-amid-virus-outbreak", "culture/india/pursuitsbugatti-centodieci-revives-spirit-of-eb110-photos-specs-price", 
+                 "tech/mobile/trai-chief-we-set-spectrum-price-industry-decides-tariff", "culture/this-is-a-title-9", "technology/youtube-now-available-for-jiophone"]
+        
+        for i in prod_paths:
+            self.open_url(url, i)
         
         # Staging
-        url_stg = "https://bloombergquint-web.qtstage.io/"
-        path5 = "bq-blue-exclusive/new-story-check-for-add-social-card"
-
-        self.open(url+path)
-        time.sleep(5)
-
-        self.open(url+path1)
-        time.sleep(5)
-
-        self.open(url+path2)
-        time.sleep(5)
-
-        self.open(url+path3)
-        time.sleep(5)
-
-        self.open(url+path4)
-        time.sleep(5)
+        url_stg = "https://malibu-integration-web.qtstage.io/"
+        stg_paths = ["whatever/imported-story-1-should-get-published-4", "whatever/headline-09837232647442259", "whatever/headline-09710416993351383", 
+                     "whatever/headline-09369913954141263", "whatever/headline-07610392921128447"]
         
-        self.open(url_stg+path5)
-        time.sleep(5)
+        for i in stg_paths:
+            self.open_url(url_stg, i)
